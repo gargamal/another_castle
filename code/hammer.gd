c969df5d174782c30c1fe63433a31cb74e07d90e
Pellier Daniel <pellier.daniel@gmail.com>
2020-06-10 16:58:19 +0200

    Gestion différente du marteau. Maintenant il revient vers le player dés qu'il touche un objet.

--- code/hammer.gd
+++ code/hammer.gd
@@ -4,38 +4,28 @@ const UP = Vector2(0, -1) # indique le plafond
 
 var speed = 1000
 var vel = Vector2()
-var has_hitting = false
-var is_fallen = false
-var pos_mem = Vector2(0, 0)
 var player
 
 func ready():
 	add_to_group("weapon")
 
 func _physics_process(delta):
-	var collision = move_and_collide(vel * delta, true, true, is_fallen)
+	var collision = move_and_collide(vel * delta, true, true, false)
 	var _velocity = move_and_slide(Vector2(0, 0), UP) # permet de fixer la normal et de connaitre la position du sol
 	
-	if collision:		
-		if not has_hitting:
-			if collision.collider.has_method("hit"):
-				collision.collider.hit(10)
-			vel = Vector2(0, speed)
-			has_hitting = true
-		elif has_hitting and not is_fallen:
-			vel = Vector2(0, 0)
-			$anim.stop()
-			$sprite.frame = 2
-			is_fallen = true
-		elif collision.collider.name == "plateform" and pos_mem == position and not is_on_floor(): 
-			player.take_hammer()
-			queue_free()
-			
+	if collision:
+		set_physics_process(false)
+		if collision.collider.has_method("hit"):
+			collision.collider.hit(10)
+		vel = Vector2(0, 0)
+		$anim.play("touch")
+		yield($anim, "animation_finished")
+		player.take_hammer()
+		queue_free()
+		
 	if player and is_not_on_limit(player.get_node("cam")):
 		player.take_hammer()
 		queue_free()
-			
-	pos_mem = position
 
 
 func is_not_on_limit(camera):
@@ -43,14 +33,11 @@ func is_not_on_limit(camera):
 		or camera.limit_top >= position.y or camera.limit_bottom <= position.y;
 
 
-func start(_player, pos, dir, diagonal):
+func start(pPlayer, pos, dir, diagonal):
 	position = pos
 	vel = Vector2(speed * dir, speed * diagonal)
-	has_hitting = false
-	is_fallen = false
-	pos_mem = Vector2(0, 0)
 	$anim.play("turn")
-	player = _player
+	player = pPlayer
 
 
 func _on_touch_box_body_entered(body):
